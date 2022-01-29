7ce6048221e25082fd399bc5af87a5890e538c19
furxx2000 <furxx2000@gmail.com>
2022-01-29 17:37:34 +0800

    刪除box-shadow

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -8,9 +8,7 @@
 .card {
   background-color: white;
   border-radius: 0.5rem;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.411);
   padding: 0.5rem;
-  margin: 2rem auto;
   max-width: 45rem;
 }
 </style>
