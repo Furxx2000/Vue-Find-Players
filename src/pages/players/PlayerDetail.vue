362bff38db7c05fa1b643fdcebd29cf15e97522e
furxx2000 <furxx2000@gmail.com>
2022-01-31 01:42:57 +0800

    html重新規劃，使用route params拿到id 取得球員資料

--- src/pages/players/PlayerDetail.vue
+++ src/pages/players/PlayerDetail.vue
@@ -1,113 +1,52 @@
 <template>
-  <section>
-    <figure>
-      <img src="" alt="" />
-      <h1>Picture</h1>
-    </figure>
-    <div class="player--info"><h1>Player--info</h1></div>
-    <div class="social--media"><h1>Social--Media</h1></div>
-  </section>
-  <div class="player--stats">
-    <div class="player-stats-left">
-      <div>PPG</div>
-      <div>RPG</div>
-      <div>ARG</div>
-      <div>PIE</div>
-    </div>
-    <div class="player-stats-right">
-      <div class="player-stats-right-item">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+  <main>
+    <h2>球員資訊</h2>
+    <section class="player--info">
+      <figure>
+        <img src="" alt="" />
+      </figure>
+      <div>
+        <div>
+          <span>#12</span>
+          <span>台北富邦勇士</span>
+          <span>後衛</span>
+        </div>
+        <div>{{ filteredPlayers.names.chiName }}</div>
       </div>
-      <div class="player-stats-right-item">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
-  </div>
+    </section>
+  </main>
 </template>
 
 <script>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+
 export default {
-  setup() {},
+  setup() {
+    const route = useRoute();
+    const store = useStore();
+
+    const players = computed(() => store.getters["players/players"]);
+
+    const filteredPlayers = computed(() =>
+      players.value.find((player) => player.id === route.params.id)
+    );
+    console.log(filteredPlayers.value);
+
+    return { route, filteredPlayers };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-section {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  height: 300px;
-  border: 1px solid black;
-  padding: 0 15rem;
-
-  figure,
-  .player--info,
-  .social--media {
-    flex-grow: 1;
-    width: 300px;
-    height: 100%;
-    border: 1px solid black;
-    margin: 0;
-    text-align: center;
-  }
+main {
+  padding: 0 1rem;
 }
 
-.player--stats {
+.player--info {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  height: 120px;
-  border: 1px solid black;
-  padding: 0 15rem;
-
-  .player-stats-left,
-  .player-stats-right {
-    flex-grow: 1;
-    border: 1px solid black;
-    height: 100%;
-  }
-
-  .player-stats-left {
-    width: 45%;
-    display: flex;
-    align-items: center;
-
-    div {
-      flex-grow: 1;
-      height: 100%;
-      border: 1px solid black;
-      text-align: center;
-      font-weight: 600;
-    }
-  }
-
-  .player-stats-right {
-    width: 55%;
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-
-    .player-stats-right-item {
-      width: 100%;
-      height: 50%;
-      border: 1px solid black;
-      display: flex;
-      align-items: center;
-
-      div {
-        flex-grow: 1;
-        height: 100%;
-        border: 1px solid black;
-      }
-    }
-  }
 }
 </style>
