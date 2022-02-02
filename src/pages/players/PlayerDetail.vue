ba72f96d1bb78b8723d45f9e1e0c90b7dbfe6b38
furxx2000 <furxx2000@gmail.com>
2022-02-02 21:28:32 +0800

    完成Player State資料

--- src/pages/players/PlayerDetail.vue
+++ src/pages/players/PlayerDetail.vue
@@ -1,24 +1,43 @@
 <template>
   <main>
     <h2>球員資訊</h2>
-    <section class="player--info">
+    <section class="player">
       <figure>
-        <img src="" alt="" />
+        <img :src="imgSrc" alt="" />
       </figure>
-      <div>
-        <div>
-          <span>#12</span>
-          <span>台北富邦勇士</span>
-          <span>後衛</span>
+      <div class="player--info">
+        <div class="player--info--upper">
+          <span>#{{ player.number }}</span>
+          <span>{{ player.team.chiTeamName }}</span>
+          <span>{{ player.position }}</span>
         </div>
-        <div>{{ filteredPlayers.names.chiName }}</div>
+        <div class="player--info--middle">{{ player.names.chiName }}</div>
+        <div class="player--info--lower">{{ player.names.engName }}</div>
       </div>
     </section>
+    <section class="player--states">
+      <div class="grid-container" :class="{ open: isOpen }">
+        <div class="item--1">得分: {{ averageData(player.points) }}</div>
+        <div class="item--2">籃板: {{ averageData(player.rebounds) }}</div>
+        <div class="item--3">助攻: {{ averageData(player.assists) }}</div>
+        <div class="item--4">身高: {{ player.height }}公分</div>
+        <div class="item--5">體重: {{ player.weight }}公斤</div>
+        <div class="item--6">年齡: {{ realAge }}歲</div>
+        <div class="item--7">職業年資: {{ player.experience }}年</div>
+        <div class="item--8">生日: {{ player.birthday }}</div>
+        <div class="item--9">國籍: {{ player.country }}</div>
+        <div class="item--10">畢業學校: {{ player.lastAttended }}</div>
+      </div>
+    </section>
+    <div @click="expand" :class="{ open: isOpen }" class="expand-arrow">
+      <img src="../../assets/icon/expand_arrow.svg" alt="" />
+    </div>
+    <div class="line"></div>
   </main>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, reactive, ref } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 
@@ -26,15 +45,60 @@ export default {
   setup() {
     const route = useRoute();
     const store = useStore();
+    const isOpen = ref(false);
 
-    const players = computed(() => store.getters["players/players"]);
+    // 篩選出球員資料
+    const selectedPlayers = computed(() => {
+      const players = computed(() => store.getters["players/players"]);
 
-    const filteredPlayers = computed(() =>
-      players.value.find((player) => player.id === route.params.id)
+      return players.value.find((player) => player.id === route.params.id);
+    });
+
+    const player = reactive({
+      ...selectedPlayers.value,
+    });
+
+    // 計算年齡
+    const realAge = computed(() => {
+      const playerBirthday = new Date(player.birthday);
+      const now = new Date();
+      const age = now.getFullYear() - playerBirthday.getFullYear();
+      const realPlayerMonth = playerBirthday.getMonth() + 1;
+      const realNowMonth = now.getMonth() + 1;
+
+      if (
+        realNowMonth < realPlayerMonth ||
+        (realNowMonth === realPlayerMonth && realNowMonth < realPlayerMonth)
+      ) {
+        return age - 1;
+      } else {
+        return age;
+      }
+    });
+
+    // 計算平均得分、籃板、助攻
+    function averageData(val) {
+      return (val / player.playedGames).toFixed(2);
+    }
+
+    const imgSrc = computed(() =>
+      require(`../../assets/pics/${player.team.engTeamName}/${route.params.id}.jpg`)
     );
-    console.log(filteredPlayers.value);
 
-    return { route, filteredPlayers };
+    // Expand-arrow控制球員State收合
+    function expand() {
+      isOpen.value = !isOpen.value;
+    }
+
+    return {
+      route,
+      player,
+      imgSrc,
+      expand,
+      isOpen,
+      averageData,
+      realAge,
+    };
   },
 };
 </script>
@@ -42,11 +106,89 @@ export default {
 <style lang="scss" scoped>
 main {
   padding: 0 1rem;
-}
 
-.player--info {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  section.player {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 2rem;
+
+    figure {
+      width: 100px;
+      height: 100px;
+      margin: 0 2rem 0 0;
+      cursor: pointer;
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 35%;
+        object-fit: cover;
+      }
+    }
+
+    .player--info--upper {
+      margin-bottom: 0.5rem;
+
+      span {
+        font-size: 18px;
+        font-weight: 600;
+        margin-right: 1rem;
+      }
+    }
+
+    .player--info--middle {
+      font-size: 1.5rem;
+      font-weight: 600;
+    }
+
+    .player--info--lower {
+      font-size: 1.1rem;
+    }
+  }
+
+  .grid-container {
+    overflow-y: hidden;
+    height: 20px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 100px;
+    column-gap: 0.5rem;
+
+    > div {
+      font-size: 15px;
+    }
+
+    > div:not(.item--1, .item--4, .item--7, .item--10) {
+      padding-left: 0.5rem;
+    }
+
+    .item--10 {
+      grid-area: 4 / 1 / 5 / 4;
+    }
+  }
+
+  .grid-container.open {
+    height: 120px;
+  }
+
+  .expand-arrow {
+    width: 40px;
+    height: 40px;
+    margin: 0 auto;
+    margin-bottom: 0.7rem;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .expand-arrow.open {
+    transform: rotate(180deg);
+  }
+
+  .line {
+    border-top: 1px solid black;
+  }
 }
 </style>
