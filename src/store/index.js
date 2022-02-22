6750ac400176d34488d68c4e3b339b26a3ae9bff
furxx2000 <furxx2000@gmail.com>
2022-02-22 14:08:03 +0800

    新增加teams module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 import playersModule from "./modules/players/index";
+import teamsModule from "./modules/teams/index";
 
 const store = createStore({
   modules: {
     players: playersModule,
+    teams: teamsModule,
   },
 });
 
