912c45dcc3a66f6aab3b58a9bae8cdefb26267e8
furxx2000 <furxx2000@gmail.com>
2022-03-11 02:18:56 +0800

    新增svg-icon元件

---
+++ src/components/UI/SvgIcon.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script>
+import { computed } from "@vue/reactivity";
+
+export default {
+  props: {
+    iconClass: {
+      type: String,
+      required: true,
+    },
+    className: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props) {
+    const iconName = computed(() => `#icon-${props.iconClass}`);
+    const svgClass = computed(() => {
+      if (props.className) {
+        return `svg-icon icon-${props.iconClass} ` + props.className;
+      } else {
+        return `svg-icon icon-${props.iconClass}`;
+      }
+    });
+
+    return { svgClass, iconName };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
