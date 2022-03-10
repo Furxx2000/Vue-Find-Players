a283b1b9c60accafd916ad7c3a0fb4be404ca9a5
furxx2000 <furxx2000@gmail.com>
2022-03-11 02:13:41 +0800

    新增全域共用SvgIcon元件, 設定webpack達成直接使用SVG

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Store from "./store/index";
 import router from "./router";
 import BaseButton from "./components/UI/BaseButton.vue";
 import BaseCard from "./components/UI/BaseCard.vue";
+import SvgIcon from "./components/UI/SvgIcon.vue";
 
 const app = createApp(App);
 
@@ -13,5 +14,12 @@ app.use(Store);
 
 app.component("base-button", BaseButton);
 app.component("base-card", BaseCard);
+app.component("svg-icon", SvgIcon);
+
+const requireAll = (requireContext) =>
+  requireContext.keys().map(requireContext);
+
+const req = require.context("./assets/icon", true, /\.svg$/);
+requireAll(req);
 
 app.mount("#app");
